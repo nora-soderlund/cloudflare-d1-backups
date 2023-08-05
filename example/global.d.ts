df24b5e5af1b11a9f8ef3889124140fd8f7d4d1d
Nora Söderlund <nora.soderlund@polestar.com>
2023-08-05 14:58:49 +0200

    Updated example

--- example/global.d.ts
+++ example/global.d.ts
@@ -1,6 +1,4 @@
 declare type Env = {
     DATABASE: D1Database;
     BUCKET: R2Bucket;
-
-    ENVIRONMENT: "production" | "staging";
 };
--- example/src/index.ts
+++ example/src/index.ts
@@ -1,6 +1,7 @@
 import { createBackup } from "@nora-soderlund/cloudflare-d1-backups";
 
 export default {
+    // ideally you'd have it in a CRON, but for the sake of debugging:
     async fetch(request: Request, env: Env, context: ExecutionContext) {
         const result = await createBackup(env.DATABASE, env.BUCKET);
 

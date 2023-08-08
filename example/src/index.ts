224370d5465cd396e9740954a15a7a6eb0b4966f
Dahoom152 <105564371+Dahoom152@users.noreply.github.com>
2023-08-08 11:00:45 +0300

    Update index.ts

--- example/src/index.ts
+++ example/src/index.ts
@@ -4,14 +4,14 @@ export default {
     // only use fetch for debugging:
     async fetch(request: Request, env: Env, context: ExecutionContext) {
         const projectName = 'test'; //add your project name here for structure
-        const result = await createBackup(env.DATABASE, env.BUCKET, {fileName :`backups/${projectName}/${(new Date()).toUTCString()}.sql`});
+        const result = await createBackup(env.DATABASE, env.BUCKET, {fileName :`backups/${porjectName}/${timeFormat()}.sql`});
 
         return Response.json(result);
     }
     async scheduled(request, env, ctx) {
         // add the CRON job in your workers dashboard panel under the "triggers" tab
         const projectName = 'test'; //add your project name here for structure
-        const result = await createBackup(env.DATABASE, env.BUCKET, {fileName :`backups/${projectName}/${(new Date()).toUTCString()}.sql`} );
+        const result = await createBackup(env.DATABASE, env.BUCKET, {fileName :`backups/${porjectName}/${timeFormat()}.sql`} );
         
         return Response.json(result);
   }
